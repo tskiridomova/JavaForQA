997e40e7cf041626df5ed888cc7cd513d39a4669
Tatiana Skiridomova <tskiridomova@gmail.com>
2021-10-08 13:54:45 -0500

    replace constants naming

--- src/main/java/lesson6/Main.java
+++ src/main/java/lesson6/Main.java
@@ -7,22 +7,22 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class Main {
-    private static final String apiUrl = "http://dataservice.accuweather.com";
-    private static final String forecastPath5Day = "/forecasts/v1/daily/5day/";
-    private static final String apiKey = "DkpkL8WfWdvUxGEP27MQ5Dv0WMG3xRGN";
-    private static final String locationKey = "295212"; // Saint-Petersburg, Russia
+    private static final String API_URL = "http://dataservice.accuweather.com";
+    private static final String FORECAST_PATH_5DAY = "/forecasts/v1/daily/5day/";
+    private static final String API_KEY = "DkpkL8WfWdvUxGEP27MQ5Dv0WMG3xRGN";
+    private static final String LOCATION_ID = "295212"; // Saint-Petersburg, Russia
 
     public static void main(String[] args) {
         // 
         StringBuilder urlBuilder = new StringBuilder();
-        urlBuilder.append(apiUrl);
-        urlBuilder.append(forecastPath5Day);
-        urlBuilder.append(locationKey);
-        urlBuilder.append("?apikey=" + apiKey);
+        urlBuilder.append(API_URL);
+        urlBuilder.append(FORECAST_PATH_5DAY);
+        urlBuilder.append(LOCATION_ID);
+        urlBuilder.append("?apikey=" + API_KEY);
         urlBuilder.append("&metric=true");
 
         System.out.println("Lesson 6");
-        System.out.println(urlBuilder.toString());
+        System.out.println(urlBuilder);
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlBuilder.toString())).build();
