b4c2dd819955dad79da1b9688135ce073986c437
Tatiana Skiridomova <tskiridomova@gmail.com>
2021-10-08 13:41:39 -0500

    lesson 7: init

---
+++ src/main/java/lesson7/MainApp.java
@@ -0,0 +1,9 @@
+package lesson7;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
